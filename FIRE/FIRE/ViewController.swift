2f7e7660e361ab3a1e90fc8b87d0e8677ab69e0a
Kelly Robinett <kellyrobinett@ip0af505ec.int.uvm.edu>
2022-10-11 10:17:49 -0400

    Created Project

---
+++ FIRE/FIRE/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FIRE
+//
+//  Created by Kelly Robinett on 10/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
