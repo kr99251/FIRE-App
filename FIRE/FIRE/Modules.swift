89b2a2caa65d28c07b53c4afcaff7b1b3561196f
Fire App Group <nljacobs@uvm.edu>
2022-10-12 16:23:12 -0400

    Added module struct, and loadjson function

--- FIRE/FIRE/Modules.swift
+++ FIRE/FIRE/Modules.swift
@@ -6,12 +6,26 @@
 //
 
 import UIKit
-
+struct Module : Codable{
+    let title : String
+    let text : String
+}
+//Parse Json idea from https://stackoverflow.com/questions/24410881/reading-in-a-json-file-using-swift
+func loadJson(filename: String) -> Module?{
+    let decoder = JSONDecoder()
+    guard
+        let json = Bundle.main.url(forResource: filename, withExtension: "json"),
+        let data = try? Data(contentsOf: json),
+        let module = try? decoder.decode(Module.self, from: data)
+    else {
+        print("Error, JSON File failed to parse")
+        return nil
+    }
+    return module
+}
 class Modules: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
