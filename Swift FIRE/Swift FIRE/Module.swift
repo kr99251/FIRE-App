dcb80adc1ede2a33fd6196a8e9492684ec2263e1
emma-shroyer <eshroyer@uvm.edu>
2022-10-26 15:58:07 -0400

    Work in modules

--- Swift FIRE/Swift FIRE/Module.swift
+++ Swift FIRE/Swift FIRE/Module.swift
@@ -7,14 +7,24 @@
 
 import SwiftUI
 
-struct Module: View {
+struct ContentView: View {
+    @State var currentValue = 0
     var body: some View {
-        
+        VStack(spacing: 20) {
+            Text("value is \(currentValue)")
+            Button("Increment", action: { currentValue += 1 } )
+            // can also put the action closure outside
+            Button("Decrement") {currentValue -= 1 }
+            
+            Button("Menu") {  }
+        }
     }
 }
 
+
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        Module()
+        ContentView()
     }
 }
