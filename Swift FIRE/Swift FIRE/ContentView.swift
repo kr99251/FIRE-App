6057dca974962cf90ad65a55380f6e2854690c76
Kelly Robinett <kellyrobinett@ip0af54cce.int.uvm.edu>
2022-10-26 15:40:46 -0400

    Added Swift FIRE

---
+++ Swift FIRE/Swift FIRE/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swift FIRE
+//
+//  Created by Kelly Robinett on 10/26/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Swift FIRE/Swift FIRE/Swift_FIREApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_FIREApp.swift
+//  Swift FIRE
+//
+//  Created by Kelly Robinett on 10/26/22.
+//
+
+import SwiftUI
+
+@main
+struct Swift_FIREApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
