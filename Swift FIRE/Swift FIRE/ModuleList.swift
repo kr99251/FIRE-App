ffa3bda1b00f0ccbff8edff5bf799f66b889d58f
emma-shroyer <eshroyer@uvm.edu>
2022-10-27 10:00:18 -0400

    Work in the list

--- Swift FIRE/Swift FIRE/ModuleList.swift
+++ Swift FIRE/Swift FIRE/ModuleList.swift
@@ -32,15 +32,43 @@ func getModules() -> ModuleDataSet{
     return ModuleDataSet(modules:[ModuleData(modId:-1, modName:"", pageMax:-1, pages:[])])
 }
 
-struct ModuleList: View {
-    var body: some View {
-        Text("Hello")
+struct ModuleListView: View {
+    var modules = getModules()
+
+    @State var currentValue = 1
+
+    
+        var body: some View {
+            let v = modules.modules
+            NavigationView {
+                VStack(alignment: .center) {
+                    Text("Modules").font(.largeTitle).padding([.bottom, .trailing, .leading], 20)
+                    VStack() {
+                        ScrollView(.vertical, showsIndicators: true) {
+                            ForEach(0..<v.count, id: \.self) {
+                                number in
+                                NavigationLink(v[number].modName, destination: ContentView())
+                                    .frame(width: 295, height: 40)
+                                    .padding(20)
+                                    .font(.largeTitle)
+                                    .foregroundColor(Color.white)
+                                    .background(Color.blue)
+                                    .cornerRadius(20)
+                            }
+                        }.padding([.top, .trailing, .leading], 10)
+                    }
+                    .frame(width: 350, height: 500)
+                    .background(Color(.systemGray4))
+
+                }
+            }
     }
 }
 
 
-struct Previews_ModuleList_Previews: PreviewProvider {
+
+struct ModuleListView_Previews: PreviewProvider {
     static var previews: some View {
-        ModuleList()
+        ModuleListView()
     }
 }
