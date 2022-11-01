e7358e18f706c049169a8a851150c4d9d1dcb97b
npd29 <npd29@comast.net>
2022-11-01 18:39:27 -0400

    Journals page displaying

---
+++ Swift FIRE/Swift FIRE/Journal.swift
@@ -0,0 +1,8 @@
+//
+//  Journal.swift
+//  Swift FIRE
+//
+//  Created by Noel Desmarais on 10/31/22.
+//
+
+import Foundation
---
+++ Swift FIRE/Swift FIRE/JournalList.swift
@@ -0,0 +1,8 @@
+//
+//  JournalList.swift
+//  Swift FIRE
+//
+//  Created by Noel Desmarais on 11/1/22.
+//
+
+import Foundation
