540c8714261bca9fbe533a18bea4c277001039f0
Fire App Group <nljacobs@uvm.edu>
2022-11-02 15:35:29 -0400

    Added fraction of size handling

--- Swift FIRE/Swift FIRE/Module.swift
+++ Swift FIRE/Swift FIRE/Module.swift
@@ -8,22 +8,27 @@
 import SwiftUI
 var modules = getModules()
 var currentModule = modules.modules[0]
+// Idea from https://stackoverflow.com/questions/57727107/how-to-get-the-iphones-screen-width-in-swiftui
+extension UIScreen{
+   static let screenWidth = UIScreen.main.bounds.size.width
+   static let screenHeight = UIScreen.main.bounds.size.height
+   static let screenSize = UIScreen.main.bounds.size
+}
 struct ContentView: View {
     @State var pageNum = 0
     //@Binding var increaseAmount : Int
     var increaseAmount = 10
     var body: some View {
         VStack{
+            Text(currentModule.pages[pageNum])
+                .font(.system(size : CGFloat(5 + increaseAmount)))
+                .frame(height: 4*UIScreen.screenHeight / 9)
             HStack{
                 Button("Next", action:{nextPage()})
-                    .offset(x:110)
-                    .offset(y:240)
+                    .offset(x: 15*UIScreen.screenWidth/32)
                 Button("Prev", action:{prevPage()})
-                    .offset(x:-110)
-                    .offset(y:240)
+                    .offset(x: -15*UIScreen.screenWidth/32)
             }
-            Text(currentModule.pages[pageNum])
-                .font(.system(size : CGFloat(25 + increaseAmount)))
         }
     }
     func nextPage(){
